4962f33106c452fbe24ff4d00cb91cf10e3c3082
BinaryWrap <binarywrap@gmail.com>
2016-12-21 15:32:14 +0530

    form validation

--- ui/src/app/product-add/product-add.component.ts
+++ ui/src/app/product-add/product-add.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormArray, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
+function matchBothSides(matchStatus: AbstractControl) {
+	return matchStatus.get('productName').value == matchStatus.get('shortDescription').value
+		? null : {'Don\'t Match': true};
+}
+
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css']
 })
+
 export class ProductAddComponent implements OnInit {
 
   form: FormGroup;
 
   constructor(public fb: FormBuilder) { 
   	this.form = this.fb.group({
-  		productName: ''
-  	});
+  		productName: ['', Validators.required],
+  		shortDescription: ['', Validators.required],
+  		productPrice: ['', Validators.required],
+  		productThumbnail: '',
+  	},
+  	{ validator: matchBothSides }
+  	);
   }
 
   ngOnInit() {
